48bd3bf1b8fee99e931d0c90c88547590e9e609a
ZihaoLiu0927 <Liuxiaohao0927@outlook.com>
2025-03-17 01:51:55 -0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' 
-    ? '/virtual_staging_demo/' 
-    : '/',
+  base: '/virtual_staging_demo/'
 })
