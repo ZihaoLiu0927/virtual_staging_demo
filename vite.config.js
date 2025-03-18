0346583177241a93ef3c7bd2e3b02620a53a5e48
ZihaoLiu0927 <Liuxiaohao0927@outlook.com>
2025-03-17 20:37:28 -0700

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/virtual_staging_demo/',
   plugins: [react()],
-})
+  base: '/virtual_staging_demo/',
+});
