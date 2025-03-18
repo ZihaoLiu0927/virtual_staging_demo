56cc748b94030541831119c0527b2a12dc5562e7
ZihaoLiu0927 <Liuxiaohao0927@outlook.com>
2025-03-17 20:11:39 -0700

    config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
   base: '/virtual_staging_demo/',
-  publicDir: 'public',
+  plugins: [react()],
 })
